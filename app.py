a604d14b766cd40a91062be44e021dd64fd0edc4
Maria B <118015089+MariaB91@users.noreply.github.com>
2025-03-10 18:02:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pdfplumber
 import tempfile
 import os
-from conversion import convertir_pdf_en_excel_conforama, convertir_pdf_en_excel_coformama_suisse, convertir_pdf_en_excel_bonami, convertir_pdf_en_excel_but  # Importation des scripts externes
+from conversion import convertir_pdf_en_excel_conforama
 
 # Sidebar
 st.sidebar.title("Sélectionner une entreprise")
